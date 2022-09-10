8c404a991f80e60907f46da7f8f6629b0cbd6af9
chrisshola1 <tkwalks1@gmail.com>
2022-09-10 11:50:15 +0100

    I just updated my menu-items to navigate with useNavigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+
+// const HomePage = props => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <Link to='/topics'>Topics</Link>
+//       <h1>HOME PAGE</h1>
+//     </div>
+//   );
+// };
+
 function App() {
   return (
     <div> 
-      <HomePage />
+     <HomePage />
+     <Routes>
+        <Route path = "/" element = {<HomePage/>}/>
+        <Route path='/hats' element={<HatsPage/>} /> 
+     </Routes>
+
+     {/* <Route path='/' component={HomePage} />
+     <Route path='/hats' component={HatsPage} /> */}
+
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+// import { withRouter } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    
-    <div className={`${size} menu-item background-image`}
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/hats');
+      };
+
+      return (
+                
+    <div className={`${size} menu-item background-image`} onclick={() => history.push(`${match.url}${linkUrl}`)}
     style={{
         backgroundImage: `url(${imageUrl})`
        // background: 'red' 
@@ -19,11 +29,13 @@ const MenuItem = ({ title, imageUrl, size }) => (
         /> */}
                 <div className='content'>
                     <h1 className='title'>{title.toUpperCase()}</h1>
-                    <span className='subtitle'>SHOP NOW</span>
+                    <span className='subtitle' onClick={handleClick}>SHOP NOW</span>
                 </div>
             </div>
+      )
+    
             
-);
+};
 
 
 export default MenuItem;
