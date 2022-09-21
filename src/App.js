3eab00c21593b8a48712e1dccd8968ff958ba77d
chrisshola1 <tkwalks1@gmail.com>
2022-09-21 12:10:56 +0100

    implemented firebase utils, included ability to store authenticated user into our firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage  from './pages/shop/shop.component';
 //import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component'
 import { SignInAndSignUpPage } from './pages/sign-in-and-sign-up/SignInAndSignUpPage';
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 // const HatsPage = () => (
 //   <div>
@@ -41,9 +41,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    console.log(auth);
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(onSnapshot => {
+          this.setState({
+            currentUser: {
+              id: onSnapshot.id,
+              ...onSnapshot.data()
+            }
+          });
+        });
+        
+      }
+       this.setState({ currentUser: userAuth });
     });
   }
 
--- src/pages/sign-in-and-sign-up/SignInAndSignUpPage.jsx
+++ src/pages/sign-in-and-sign-up/SignInAndSignUpPage.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 //import { SignIn } from '../../components/sign-in/sign-in-component';
 import { SignIn } from '../../components/sign-in/SignIn'
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss';
 export const SignInAndSignUpPage = () => {
     return (
-        <div className='sign-in-and-sign-up'><SignIn/></div>
+        <div className='sign-in-and-sign-up'>
+            <SignIn/>
+            <SignUp/>
+            </div>
     );
     
 }
 
-//export default SignInAndSignUpPage;
+export default SignInAndSignUpPage;
